54f103b00dd64b6db72b10b469ea71313575fb36
mohamed-bahaj <Mohamed.BAHAJ@student.umons.ac.be>
2017-10-20 14:12:05 +0200

    exercice 2

--- src/main/java/exercise2/Exercise2.java
+++ src/main/java/exercise2/Exercise2.java
@@ -8,22 +8,18 @@ public class Exercise2 {
 	// Implémentez-y une méthode statique "resetPopulation" pour remettre les compteurs à zéro.
 
 	static void createPerson(int age) {
+		new Person(age);
 	}
 
 	static int computePopulationSize() {
-		// TODO remove comment when implemented
-		// return Person.computePopulationSize();
-		return 0;
+		return Person.getInstances();
 	}
 
 	static float computeAveragePopulationAge() {
-		// TODO remove comment when implemented
-		// return Person.computeAveragePopulationAge();
-		return 0.0F;
+		return Person.getTotalAge();
 	}
 
 	static void resetPopulation() {
-		// TODO remove comment when implemented
-		// Person.resetPopulation();
+		Person.resetPopulation();
 	}
 }
--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,26 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int instances=0;
+	private static int totalAge=0;
+
+	public Person(int age) {
+		this.age = age;
+		instances++;
+	}
+
+	public static int getInstances(){
+		return Person.instances;
+	}
+
+	public static int getTotalAge(){
+		return (int) (Person.totalAge/Person.instances);
+	}
+
+	public static void resetPopulation(){
+		Person.totalAge=0;
+		Person.instances=0;
+	}
+
 }
